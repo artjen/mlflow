e7ae671001689e01b392a341c61f61e5d45c782d
Arthur Jenoudet <arthur.jenoudet@databricks.com>
2024-03-21 19:31:08 -0700

    use presigned urls by default behind env var and update listing to use non-deprecated api
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -536,3 +536,7 @@ _MLFLOW_RUN_SLOW_TESTS = _BooleanEnvironmentVariable("MLFLOW_RUN_SLOW_TESTS", Fa
 #: The OpenJDK version to install in the Docker image used for MLflow models.
 #: (default: ``11``)
 MLFLOW_DOCKER_OPENJDK_VERSION = _EnvironmentVariable("MLFLOW_DOCKER_OPENJDK_VERSION", str, "11")
+
+MLFLOW_UNITY_CATALOG_PRESIGNED_URLS_ENABLED = _BooleanEnvironmentVariable(
+    "MLFLOW_UNITY_CATALOG_PRESIGNED_URLS_ENABLED", False
+)
