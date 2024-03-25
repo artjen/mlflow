ce6773c8fe46774fd5e7896d89c25e9751ba7ade
Arthur Jenoudet <arthur.jenoudet@databricks.com>
2024-03-25 12:10:43 -0700

    with upload / download test

--- tests/store/artifact/test_presigned_url_artifact_repo.py
+++ tests/store/artifact/test_presigned_url_artifact_repo.py
@@ -4,10 +4,14 @@ from unittest import mock
 from unittest.mock import ANY
 
 import requests
+from sklearn.ensemble import RandomForestClassifier
+from tensorflow.python.tpu import datasets
 
+import mlflow
 from mlflow.protos.databricks_artifacts_pb2 import ArtifactCredentialInfo
 from mlflow.protos.databricks_filesystem_service_pb2 import ListDirectoryResponse, DirectoryEntry, HttpHeader, \
     CreateDownloadUrlResponse, CreateDownloadUrlRequest, CreateUploadUrlResponse, CreateUploadUrlRequest
+from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from mlflow.store.artifact.presigned_url_artifact_repo import PresignedUrlArtifactRepository, DIRECTORIES_ENDPOINT
 from mlflow.utils.proto_json_utils import message_to_json
 
@@ -209,3 +213,37 @@ def test_upload_to_cloud(tmp_path):
                                            data=bytearray(content, "utf-8"),
                                            headers=_make_headers(remote_file_path))
         mock_status.assert_called_once_with(resp.__enter__())
+
+
+def test_download_artifacts():
+    os.environ["MLFLOW_UNITY_CATALOG_PRESIGNED_URLS_ENABLED"] = "True"
+
+    mlflow.set_registry_uri("databricks-uc://arclight-prototype")
+
+    # uri = mlflow.MlflowClient().get_model_version_download_uri("artjen.test.newtestmodel", "2")
+    # ModelsArtifactRepository(uri).download_artifacts(artifact_path="")
+    ModelsArtifactRepository("models:/main.default.newtestmodel/2").download_artifacts(artifact_path="")
+
+
+def test_upload_artifacts(tmp_path):
+    from sklearn import datasets
+    from sklearn.ensemble import RandomForestClassifier
+
+    os.environ["MLFLOW_UNITY_CATALOG_PRESIGNED_URLS_ENABLED"] = "True"
+    mlflow.set_registry_uri("databricks-uc://arclight-prototype")
+
+    with mlflow.start_run():
+        # Train a sklearn model on the iris dataset
+        X, y = datasets.load_iris(return_X_y=True, as_frame=True)
+        clf = RandomForestClassifier(max_depth=7)
+        clf.fit(X, y)
+        # Take the first row of the training dataset as the model input example.
+        input_example = X.iloc[[0]]
+        # Log the model and register it as a new version in UC.
+        mlflow.sklearn.log_model(
+            sk_model=clf,
+            artifact_path="model",
+            # The signature is automatically inferred from the input example and its predicted output.
+            input_example=input_example,
+            registered_model_name="main.default.newtestmodel",
+        )
